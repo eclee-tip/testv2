87767feb917f9a8e1a71e1980690fcd03cd85385
eclee-tip <eclee-tip>
2023-03-19 18:52:34 +0800

    Graph for ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Line } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip
+} from 'chart.js';
+
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip
+)
 
 function App() {
+  const data = {
+    labels: ['1','2','3'],
+    datasets: [{
+      label: 'Numbers',
+      data: [10,6,9],
+      backgroundColor: 'red',
+      borderColor: 'black',
+      pointBorderColor: 'aqua',
+      fill: true,
+      tension: 0.4
+      }
+    ]
+  }
+
+  const options = {
+    plugins: {
+      legend: true
+    },
+    scales: {
+      y: {
+        // min: 3,
+        // max: 50
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Temperature Readings</h1>
+      <div style={
+        {
+        width: '600px',
+        height: '300px',
+        padding: '20px'
+        }
+      }>
+      <Line
+        data={data}
+        options={options}
+      ></Line>
+      </div>
     </div>
   );
 }
